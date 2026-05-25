97c81a852038ae31a181bf875fcc2a7978fffb19
Xims22 <xluna966@gmail.com>
2026-05-25 01:02:23 -0600

    Primera modificacion : archivo MayorEdad.c

--- Viernes13Meyo/MayorEdad.c
+++ Viernes13Meyo/MayorEdad.c
@@ -1,1 +1,34 @@
 #include <stdio.h>
+
+int main() {
+    int edad;
+    float promedio;
+    int reprobadas;
+    int faltas;
+
+    printf("Edad: ");
+    scanf("%d", &edad);
+
+    printf("Promedio: ");
+    scanf("%f", &promedio);
+
+    printf("Materias reprobadas: ");
+    scanf("%d", &reprobadas);
+
+    printf("Faltas acumuladas: ");
+    scanf("%d", &faltas);
+
+    if (reprobadas >= 4)
+        printf("No puedes reinscribirte\n");
+    else if (reprobadas <= 2)
+        printf("Puedes reinscribirte\n");
+    else if (reprobadas == 3)
+        printf("Revision especial\n");
+
+    if (promedio >= 9 && reprobadas == 0 && faltas < 5)
+        printf("Eres elegible para solicitar la beca\n");
+    else
+        printf("No aplicas para solicitar la beca\n");
+
+    return 0;
+}
